c0197bac7b5e2f955ba711e2a2cc9fb506f12dbe
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-11-01 23:34:06 +0200

    ProfileHeader added

---
+++ lib/widgets/profile_header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatefulWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  State<ProfileHeader> createState() => _ProfileHeaderState();
+}
+
+class _ProfileHeaderState extends State<ProfileHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
