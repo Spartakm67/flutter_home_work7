18e97d4667eb6a32eca65ed1dd7c9925d14ff588
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-11-02 15:19:09 +0200

    profile_page.dart updated

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,44 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_home_work7/widgets/profile_header.dart';
 
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
 
- class ProfilePage extends StatefulWidget {
-   const ProfilePage({super.key});
- 
-   @override
-   State<ProfilePage> createState() => _ProfilePageState();
- }
- 
- class _ProfilePageState extends State<ProfilePage> {
-   @override
-   Widget build(BuildContext context) {
-     return const ProfileHeader();
-   }
- }
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
 
+class _ProfilePageState extends State<ProfilePage> {
+  String name = "Spartak Maznytskyi";
+  String work = "Flutter Developer";
 
-// String name = "John Doe";
-// String email = "johndoe@example.com";
-//
-// void updateProfile(String newName, String newEmail) {
-//   setState(() {
-//     name = newName;
-//     email = newEmail;
-//   });
-// }
-//
-// @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     appBar: AppBar(
-//       title: const Text('User Profile'),
-//     ),
-//     body: LayoutBuilder(
-//       builder: (context, constraints) {
-//         return SingleChildScrollView(
-//           child: Column(
-//             children: [
-//               const ProfileHeader(),
+  void updateProfile(String newName, String newWork) {
+    setState(() {
+      name = newName;
+      work = newWork;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Profile'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Column(children: [
+              const ProfileHeader(),
+              ]
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
 //               ProfileDetails(
 //                 name: name,
 //                 email: email,
@@ -66,8 +65,7 @@ import 'package:flutter_home_work7/widgets/profile_header.dart';
 //     ),
 //   );
 // }
-//
+
 // Widget _buildTextWidget(String text) {
 //   return Text(text);
-// }
 // }
